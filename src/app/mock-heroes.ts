e58e6474482d7f9adcc9bb5c4ba641d2fddc62e1
manikanta <manikanta@villageit.services>
2019-10-15 16:46:35 +0530

    [TASK]- added project to git

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Manikanta' },
+  { id: 12, name: 'Shiva' },
+  { id: 13, name: 'Ravi-Teja' },
+  { id: 14, name: 'Subhani' },
+  { id: 15, name: 'Narayana' },
+  { id: 16, name: 'Nani' },
+  { id: 17, name: 'Madhu' },
+  { id: 18, name: 'Ramya' },
+  { id: 19, name: 'Teja' },
+  { id: 20, name: 'Kumar' }
+];
